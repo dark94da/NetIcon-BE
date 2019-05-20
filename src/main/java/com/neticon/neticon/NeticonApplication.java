338aa2af655dd6cba66ea5450e28f56d37788241
daruike <daruike@pinduoduo.com>
2019-05-20 20:21:33 +0800

    project initiated.

---
+++ src/main/java/com/neticon/neticon/NeticonApplication.java
@@ -0,0 +1,13 @@
+package com.neticon.neticon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeticonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeticonApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/neticon/neticon/datasource/H2DataSource.java
@@ -0,0 +1,20 @@
+package com.neticon.neticon.datasource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class H2DataSource {
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2)
+                .setScriptEncoding("UTF-8")
+                .addScript("h2/mode.sql")
+                .setScriptEncoding("UTF-8")
+                .build();
+    }
+}
